c8fc2b9279847c2eec3ec5176b526f7893cfea2e
Álvaro Rodríguez Pardo <80789216+alvarorp22UOC@users.noreply.github.com>
2021-04-06 09:33:43 +0100

    Update diputacion.py

--- Code/diputacion.py
+++ Code/diputacion.py
@@ -118,7 +118,7 @@ subastas_pendientes = pd.DataFrame(
 )
 
 # Guardamos el dataframe en un archivo CSV
-subastas_pendientes.to_csv("data/subastas_pendientes.csv")
+subastas_pendientes.to_csv("Datasets/data/subastas_pendientes.csv")
 
 
 
@@ -248,4 +248,4 @@ subastas_resueltas = pd.DataFrame(
 )
 
 # Guardamos el dataframe en un archivo CSV
-subastas_resueltas.to_csv("data/subastas_resueltas.csv")
+subastas_resueltas.to_csv("Datasets/data/subastas_resueltas.csv")
